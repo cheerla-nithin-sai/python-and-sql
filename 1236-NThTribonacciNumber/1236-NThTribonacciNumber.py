e3ad98a4bfa2e6ea3c72fc5e2f6faaf6bd8b145c
Cheerla nithinsai <154802280+cheerla-nithin-sai@users.noreply.github.com>
2026-04-20 13:27:29 +0530

    Create 1236-NThTribonacciNumber

---
+++ 1236-NThTribonacciNumber/1236-NThTribonacciNumber.py
@@ -0,0 +1,14 @@
+# Last updated: 4/20/2026, 1:27:30 PM
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n==0:
+            return 0
+        elif n==1 or n==2:
+            return 1
+        dp=[-1]*(n+1)
+        dp[0]=0
+        dp[1]=1
+        dp[2]=1
+        for i in range(3,n+1):
+            dp[i]=dp[i-2]+dp[i-1]+dp[i-3]
+        return dp[n]
