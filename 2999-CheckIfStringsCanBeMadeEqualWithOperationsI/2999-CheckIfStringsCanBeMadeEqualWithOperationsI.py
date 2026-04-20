d6ec072aadf96b75b8c191cdac5378d08cd0691d
Cheerla nithinsai <154802280+cheerla-nithin-sai@users.noreply.github.com>
2026-04-20 13:24:49 +0530

    Create 2999-CheckIfStringsCanBeMadeEqualWithOperationsI

---
+++ 2999-CheckIfStringsCanBeMadeEqualWithOperationsI/2999-CheckIfStringsCanBeMadeEqualWithOperationsI.py
@@ -0,0 +1,8 @@
+# Last updated: 4/20/2026, 1:24:49 PM
+class Solution:
+    def canBeEqual(self, s1: str, s2: str) -> bool:
+        a=sorted([s1[0],s1[2]])
+        b=sorted([s1[1],s1[3]])
+        c=sorted([s2[0],s2[2]])
+        d=sorted([s2[1],s2[3]])
+        return a==c and b==d
