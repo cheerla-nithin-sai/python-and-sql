d4eeeae2d62ec1ec70e983dfce7ea9a57125b1f1
Cheerla nithinsai <154802280+cheerla-nithin-sai@users.noreply.github.com>
2026-04-20 13:25:19 +0530

    Create 2127-EmployeesWhoseManagerLeftTheCompany

---
+++ 2127-EmployeesWhoseManagerLeftTheCompany/2127-EmployeesWhoseManagerLeftTheCompany.sql
@@ -0,0 +1,5 @@
+-- Last updated: 4/20/2026, 1:25:20 PM
+# Write your MySQL query statement below
+select employee_id from employees
+where salary<30000 and manager_id not in (select employee_id from employees)
+order by  employee_id
