50cd76e3e35d7102e1c552e085dd4888ebfcd00d
Cheerla nithinsai <154802280+cheerla-nithin-sai@users.noreply.github.com>
2026-04-20 13:27:38 +0530

    Create 1146-GreatestCommonDivisorOfStrings

---
+++ 1146-GreatestCommonDivisorOfStrings/1146-GreatestCommonDivisorOfStrings.py
@@ -0,0 +1,9 @@
+# Last updated: 4/20/2026, 1:27:39 PM
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1+str2!=str2+str1:
+            return ""
+        k = gcd(len(str1),len(str2))
+        return str1[:k]
+    
+        
