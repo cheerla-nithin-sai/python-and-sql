8bdf64eb91f9a0b1fe142faa667e95adced2b1a1
Cheerla nithinsai <154802280+cheerla-nithin-sai@users.noreply.github.com>
2026-04-20 13:24:20 +0530

    Create 3803-FindProductsWithValidSerialNumbers

---
+++ 3803-FindProductsWithValidSerialNumbers/3803-FindProductsWithValidSerialNumbers.sql
@@ -0,0 +1,4 @@
+-- Last updated: 4/20/2026, 1:24:20 PM
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select * from products where description like '% SN____-____' or description like '% SN____-____ %'
