cf3083a1961900e8840bc20d54c9ced7d48a405a
Cheerla nithinsai <154802280+cheerla-nithin-sai@users.noreply.github.com>
2026-04-21 08:41:22 +0530

    Create 232-ImplementQueueUsingStacks

---
+++ 232-ImplementQueueUsingStacks/232-ImplementQueueUsingStacks.py
@@ -0,0 +1,33 @@
+# Last updated: 4/21/2026, 8:41:22 AM
+class MyQueue:
+
+    def __init__(self):
+        self.stack1=[]
+        self.stack2=[]
+
+    def push(self, x: int) -> None:
+        self.stack1.append(x)
+        
+    def pop(self) -> int:
+        if len(self.stack1)>0:
+            return self.stack1.pop(0)
+        else:
+            return -1
+        
+    def peek(self) -> int:
+        if len(self.stack1)>0:
+            return self.stack1[0]
+        else:
+            return -1
+        
+    def empty(self) -> bool:
+        return len(self.stack1)==0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
