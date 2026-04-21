993e9c6062a1702f2d032f7e3139b0b6fb57d9a2
Cheerla nithinsai <154802280+cheerla-nithin-sai@users.noreply.github.com>
2026-04-21 08:41:07 +0530

    Create 349-IntersectionOfTwoArrays

---
+++ 349-IntersectionOfTwoArrays/349-IntersectionOfTwoArrays.py
@@ -0,0 +1,9 @@
+# Last updated: 4/21/2026, 8:41:06 AM
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        l = []
+        for i in nums1:
+            if i in nums2 and i not in l:
+                l.append(i)
+        return l
+        
