182b48ca8c60645d5385e36e7c334a22591fa7a1
Cheerla nithinsai <154802280+cheerla-nithin-sai@users.noreply.github.com>
2026-04-21 08:42:49 +0530

    Create 26-RemoveDuplicatesFromSortedArray

---
+++ 26-RemoveDuplicatesFromSortedArray/26-RemoveDuplicatesFromSortedArray.py
@@ -0,0 +1,9 @@
+# Last updated: 4/21/2026, 8:42:49 AM
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i=0
+        for j in range(1,len(nums)):
+            if nums[i]!=nums[j]:
+                i+=1
+                nums[i]=nums[j]
+        return i+1
